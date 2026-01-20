f56094fe0f04b4350563122464c4010f10a4c36d
Benjamin Große <Benjamin@grossesippe.de>
2026-01-20 22:23:27 +0100

    cleand up

--- cmd/version.go
+++ cmd/version.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/GrosseBen/spgtty/pkg/utils"
 	"github.com/spf13/cobra"
 	"log"
@@ -11,17 +10,12 @@ import (
 func version(cmd *cobra.Command, args []string) error {
 	versionFlag, err := cmd.PersistentFlags().GetBool("version")
 	if err != nil {
-		return fmt.Errorf("Fehler beim Abrufen des 'version'-Flags: %w", err)
+		log.Fatalf("Fehler beim Abrufen des 'version'-Flags: %w", err)
 	}
 	if versionFlag {
 		utils.PrintVersion()
 		os.Exit(0) // Beendet das Programm, nachdem die Version angezeigt wurde
 	}
 
-	notMinimizeFlagValue, err = cmd.PersistentFlags().GetBool("notMinimize")
-	if err != nil {
-		log.Fatalf("notMinimize failed %v ", err)
-	}
-
-	return nil
+	return err
 }
