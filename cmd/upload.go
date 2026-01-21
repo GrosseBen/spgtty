92dec3cd4b0f1dbca85742b2bead95a17b0c4ccc
Benjamin Große <Benjamin@grossesippe.de>
2026-01-21 18:10:57 +0100

    one more detail

--- cmd/upload.go
+++ cmd/upload.go
@@ -1,18 +1,24 @@
 package cmd
 
 import (
+	"encoding/json"
+	"log"
+
 	"github.com/spf13/cobra"
 )
 
 // Definiere dein Cobra-Befehlsobjekt für den Upload
 var uploadCmd = &cobra.Command{
-	Use:   "upload", // Der Name des Unterbefehls, den der Benutzer eingibt (z.B. "yourcli upload")
+	Use:   "upload [http://<hostname or IP>] [<file.js>]", // Der Name des Unterbefehls, den der Benutzer eingibt (z.B. "yourcli upload")
 	Short: "Uploads files to a specified destination",
 	Long:  `This command handles the uploading of various file types to a remote server or service.`,
-	//Args:  cobra.MinARGES(1),
-	Run: upload, // Hier wird deine 'upload'-Funktion als Run-Feld zugewiesen
+	Args:  cobra.OnlyValidArgs,
+	Run:   upload, // Hier wird deine 'upload'-Funktion als Run-Feld zugewiesen
+	//toDo: add script ID
 }
 
 func upload(cmd *cobra.Command, args []string) {
+	j, _ := json.Marshal(args)
+	log.Println("upload ", string(j))
 	panic("upload not jet inmplemented")
 }
